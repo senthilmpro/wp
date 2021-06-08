5c772e661180138e244fdbb98b3cee87628a1593
senthilmpro <m.senthil.000@gmail.com>
2021-06-07 21:54:53 -0700

    updated clean title

--- src/components/ArchiveItem/ArchiveItem.js
+++ src/components/ArchiveItem/ArchiveItem.js
@@ -28,12 +28,32 @@ const ArchiveItem = ({ title, url }) => {
         }));
     }
 
+    let filters = [`www-tamilblasters-re-`,`www-tamil-blasters-pl-`,`www-tamilblasters-re-`,`www-tamil-blasters-one-`,`www-1-tamil-mv-mx-`,'-torrent','www-tamil-blasters-ws-','www-tamil-blasters-nl-',
+        'www-tamil-blasters-me-','www-tamilblasters-me-','www-tamil-blasters-re-','www-tamil-blasters-uk-','www-tmil-blasters-me-','www-tamil-blasters-net-'];
+    const cleanTitle = (title) => {
+        let strCopy = new String(title);
+        try {
+            filters.forEach(x => {
+                title = title.replace(x,'');
+            });
+            // check for year.
+            let match = title.match(/((19|20)(\d{2}))/);
+            if(match && match.length > 0){
+                let splits = title.split("-"+ match[0]+ "-");
+                title = splits[0] + ` (${match[0]}) `+ splits[1];
+            }
+            return title;
+        } catch(err) {
+            return strCopy;
+        }
+    }
+
     return (
         <div className="LinkItem">
             <div className="TitleItem">
                 <div>
                     <span className="dot"></span>
-                    <a href={url} target="_blank">{title}</a>
+                    <a href={url} target="_blank">{cleanTitle(title)}</a>
                 </div>
                 <button className="btn btn-default btn-sm links-btn"
                     onClick={() => fetchArchiveUrl(url)}>
