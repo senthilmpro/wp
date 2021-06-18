9202987741703b5149180e0cb3aa37945cc6f145
Senthil Muthuvel <m.senthil.000@gmail.com>
2021-06-18 10:13:24 -0500

    remove search value from dependencies

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -40,7 +40,7 @@ const Home = () => {
             setArchiveLinks(pageIndex);
         }
         // eslint-disable-next-line
-    }, [pageIndex, searchValue])
+    }, [pageIndex])
 
     return (
         <div className="container Home">
